826b76065a1c9e45f401d0893c2631e53c5d7c8b
Durga <nagasaidurgam@gmail.com>
2026-04-03 13:02:31 +0530

    creating server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app= express();
+
+app.use(express.json());
+
+//routes 
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/Restaurants", require("./routes/restaurantRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+//test routes
+app.get("/", (req, res) => {
+    res.send("API RUNNING");
+});
+
+//server start
+const port = process.env.PORT || 5000;
+
+app.listen (process.env.PORT,() => {
+console.log(`server running on port ${process.env.PORT}`);
+})
+
+// npm init -y, npm i express, npm i nodemon 
